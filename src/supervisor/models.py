80adbc9a69b7667f388783d7eadba6b9abf97677
Jake Adams <jdadams@utah.gov>
2020-12-18 11:53:41 -0700

    docs: models

--- src/supervisor/models.py
+++ src/supervisor/models.py
@@ -11,8 +11,25 @@ from .message_handlers import ConsoleHandler, MessageHandler
 
 
 class Supervisor:
-    """
-    Primary class; has the replacement error handler and a Messenger object to handle messaging
+    """Captures exceptions and sends these and other notifications via email or other methods.
+
+    Attributes
+    ----------
+    project_name : str
+        The name of the parent project using Supervisor; used to get it's logfile
+    message_handlers : [MessageHandler]
+        Notifications will be sent via all handlers in this list
+    log_path : Path
+        Path to the logfile
+
+    Methods
+    -------
+    add_message_handler(handler)
+        Register a new handler for sending messages
+    notify(message)
+        Send a notification to all registered handlers
+    _manage_exceptions
+        Closure around a replacement for sys.excepthook
     """
 
     def __init__(self, project_name, log_path=None):
@@ -27,31 +44,40 @@ class Supervisor:
         sys.excepthook = self._manage_exceptions()
 
     def add_message_handler(self, handler: MessageHandler):
-        """
-        Adds a message handler to the list of handlers
+        """Register a new handler for sending messages
+
+        Parameters:
+        -----------
+        handler : MessageHandler
+            A MessageHandler instance for the desired notification method.
         """
         self.message_handlers.append(handler)
 
     def notify(self, message):
-        """
-        Calls the .send_message() method on every handler
-        """
+        """Send a notification to all registered handlers by calling .send_message() """
         for handler in self.message_handlers:
             handler.send_message(message)
 
     def _manage_exceptions(self):
-        """
-        A closure so that global_exception_handler() has access to self.notify to send notifications while still
+        """Closure around a replacement for sys.excepthook
+
+        A closure ensures global_exception_handler() has access to self.notify to send notifications while still
         maintaining the signature required by sys.excepthook
         """
 
         log = logging.getLogger(self.project_name)
 
         def global_exception_handler(exc_class, exc_object, tb):  # pylint: disable=invalid-name
-            """
-            exc_class: the type of the exception
-            exc_object: the exception object
-            tb: Traceback
+            """Log any uncaught exceptions and send a notification via self.notify
+
+            Parameters
+            ----------
+            exc_class : Type
+                The type of the exception
+            exc_object : Exception
+                The exception object
+            tb : Traceback
+                Original traceback
 
             Used to handle any uncaught exceptions. Formats an error message, logs it, and sends an email.
             """
@@ -79,8 +105,20 @@ class Supervisor:
 
 
 class MessageDetails:  # pylint: disable=too-few-public-methods
-    """
-    Data structure for message details to allow dot-access and null item checks.
+    """Data structure for message details to allow dot-access and null item checks.
+
+    Attributes
+    ----------
+    message : str
+        The text of the message
+    attachment : list
+        Strings or Paths to any attachments
+    log_file : Path
+        Path to an on-disk log file
+    subject : str
+        The message subject
+    project_name : str
+        The name of the project that has added the Supervisor object. Used for adding the version to notifications.
 
     TODO: Implement true Null-Object pattern.
     """
