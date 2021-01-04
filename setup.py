a214bbc640b5e96d3bd88e888502340887a07089
Jake Adams <jdadams@utah.gov>
2021-01-04 15:08:26 -0700

    version bump for logging fixes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='supervisor',
-    version='1.0.0',
+    version='1.1.0',
     license='MIT',
     description='A watchdog module for scheduled scripts that sends notifications, including any uncaught exceptions.',
     author='Jake Adams, AGRC',
