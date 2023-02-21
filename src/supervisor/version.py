6a15e2291bf649226aac2f42c450c76ca64854d2
Jake Adams <jdadams@utah.gov>
2023-02-21 11:30:35 -0700

    ci: auto-publish on tag

--- src/supervisor/version.py
+++ src/supervisor/version.py
@@ -2,4 +2,5 @@
 This must only include a single line: __version__ = 'x.y.z'
 """
 
+#: This will be automatically set by the publish action when it is uploaded to pypi; only change this for local dev
 __version__ = '3.0.2'
