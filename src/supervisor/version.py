6c15a8640134be09a44ebc0feaf53adbe801f939
Jake Adams <jdadams@utah.gov>
2023-02-22 08:15:56 -0700

    chore: version changes

--- src/supervisor/version.py
+++ src/supervisor/version.py
@@ -3,4 +3,4 @@ This must only include a single line: __version__ = 'x.y.z'
 """
 
 #: This will be automatically set by the publish action when it is uploaded to pypi; only change this for local dev
-__version__ = '3.0.2'
+__version__ = '0.0.0'
